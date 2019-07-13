f574aadbdbba52b2e0b4ea3d9dc5447f4d773e9c
Steve <stevetasticsteve@gmail.com>
2019-07-13 19:30:01 +1000

    Removed circular import from MapGenerator

--- MapGenerator.py
+++ MapGenerator.py
@@ -5,15 +5,18 @@ import Main
 class map:
     def __init__(self):
         tile_size = 64
-        map_width = Main.Window_width
-        map_height = Main.Window_height
+        self.map_width = 800
+        self.map_height = 600
 
         tiles = ['Assets/Tiles/' + x for x in os.listdir('Assets/Tiles') if x.endswith('.png')]
-        horizontal_tiles_number = int(map_width/tile_size)
-        vertical_tiles_number = int(map_height/tile_size)
+        horizontal_tiles_number = int(self.map_width/tile_size)
+        vertical_tiles_number = int(self.map_height/tile_size)
+
+        self.tile_coordinates = []
 
         self.tile_map = {}
 
         for column in range(horizontal_tiles_number):
             for row in range(vertical_tiles_number):
-                self.tile_map[(column, row)] = random.choice(tiles)
+                tile_coordinate = (column, row)
+                self.tile_map[tile_coordinate] = random.choice(tiles)
